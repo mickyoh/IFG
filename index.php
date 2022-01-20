df68d91774b15d97594d95cb57d9dfa7cdf2ff47
mitsuki <96933082+mickyoh@users.noreply.github.com>
2022-01-20 16:39:41 +0900

    Create index.php

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>IFG music band</title>
+    <link rel="stylesheet" href="stylesheet.css">
+    <link rel="stylesheet" href="responsive.css">
+  </head>
+  <body>
+    <h1 class="title">IFG<br>  ～boys music band～</h1>
+    　<p>世界中から集結したプロミュージシャン達</p>
+    　<h2>■メンバー紹介■</h2>
+    　　<h3>◎<u>ボーカル担当　しのさん</u></h3>
+    　　　<p>全米が泣いた奇跡の低音。聞いた者をメロメロにさせる力をもつ。通称 "泣かせのしの" 。</p>  
+    　　　<img src="vo.shi.JPG" alt="">
+    　　<h3>◎<u>ドラム担当　いのっち</u></h3>
+    　　　<p>現役のイタコ。現在もパット・トーピーが乗り移っている。</p>
+    　　　<img src="dr.JPG" alt="">
+    　　<h3>◎<u>ベース担当　五月女ぽいって</u></h3>
+    　　　<p>某鉄道会社のレール整備士。「レールの響きはベースも同じ、祇園精舎の鐘の音。」ポイってよ！</p>
+    　　　<img src="ba.JPG" alt="">
+    　　<h3>◎<u>ギター担当　Mitsuoh-VH</u></h3>
+    　　　<p>中東で油田採掘中に穴に落下してから記憶喪失。</p>
+    　　　<img src="gui.JPG" alt="">
+    　　<h3>◎<u>マネージャー担当 ミッチャン</u></h3>
+    　　　<p>Cool bearty!</p>
+    　　　<img src="micky.JPG" alt="">
+    　　<h3>◎<u>マネージャー担当 サヲリ</u></h3>
+    　　　<p>スナック始めました!</p>
+    　　　<img src="sao.jpg" alt="">
+
+    <h2>メディア取材連絡先</h2>
+    <p>メールアドレス(必須)</p>
+    <input>
+    <p>お問い合わせ内容(必須)</p>
+    <textarea></textarea>
+    
+
+  </body>
+</html>
